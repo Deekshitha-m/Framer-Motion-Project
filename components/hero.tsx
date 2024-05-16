d69c9c9bd941cf4e1fae4cf64dd0ae63c17465d8
Deekshitha-m <deekshitham12052001@gmail.com>
2024-05-16 15:05:06 +0530

    .

---
+++ components/hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    <section className='h-[60vh] lg:h-[100vh] bg-hero bg-cover bg-no-repeat bg-center'>
+        <div className='text-center'>hero</div>
+    </section>
+    </>
+  )
+}
+
+export default Hero
